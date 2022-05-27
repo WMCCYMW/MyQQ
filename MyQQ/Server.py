ceaf331d132361eaa2d9c4873b46c4a3feb995da
WMCCYMW <100657002+WMCCYMW@users.noreply.github.com>
2022-05-27 16:33:00 +0800

    添加了一些注释以及修改了部分语法错误

--- MyQQ/Server.py
+++ MyQQ/Server.py
@@ -6,15 +6,15 @@ def LoginSever(handler):
     password  = str(handler.connection.recv(1024).decode())
     isSucceed = SqlServer.LoginHandler.login_check(name, password)
     if(isSucceed == 1):
-        handler.connection.sendAll(bytes(str(isSucceed),"utf-8"))
+        handler.connection.sendall(bytes(str(isSucceed),"utf-8"))
         return [isSucceed, name]
     else:
-        handler.connection.sendAll(bytes(str(isSucceed),"utf-8"))
+        handler.connection.sendall(bytes(str(isSucceed),"utf-8"))
 
-
+# 注册函数
 def RegistSever(handler):
     # 获取name和password
     name = str(handler.connection.recv(1024).decode())
     password = str(handler.connection.recv(1024).decode())
     isSucceed =  SqlServer.LoginHandler.create_new_user(name, password)
-    handler.connection.sendAll(bytes(str(isSucceed),"utf-8"))
+    handler.connection.sendall(bytes(str(isSucceed),"utf-8"))
